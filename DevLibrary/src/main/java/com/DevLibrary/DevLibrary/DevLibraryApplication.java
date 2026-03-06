053c713ab7cfa42cdeb7dcc55d09bbf134eb25c1
Hossam alzahrani <hossamal.zahrani944@gmail.com>
2026-03-06 21:41:44 +0300

    init project and close #1

---
+++ DevLibrary/src/main/java/com/DevLibrary/DevLibrary/DevLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.DevLibrary.DevLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevLibraryApplication.class, args);
+	}
+
+}
