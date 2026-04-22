5aff931caa33394f07927add2c238d6d646c8c50
Hossam alzahrani <hossamal.zahrani944@gmail.com>
2026-04-22 09:18:59 +0300

    complete adding cloud database and close #16

--- DevLibrary/src/main/java/com/DevLibrary/DevLibraryApplication.java
+++ DevLibrary/src/main/java/com/DevLibrary/DevLibraryApplication.java
@@ -7,27 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DevLibraryApplication {
     public static void main(String[] args) {
         SpringApplication.run(DevLibraryApplication.class, args);
-/*
-example for the using of builder:
-BookResource book = new BookResource.Builder(
-        "Clean Code",
-        "CPIT-252",
-        "https://example.com/book"
-)
-.description("Good programming book")
-.author("Robert Martin")
-.build();
-
-without author is:
-BookResource book = new BookResource.Builder(
-        "Java Basics",
-        "CPIT-252",
-        "https://example.com/java"
-).build();
-
-
-
- */
+        
 	}
 }
 
