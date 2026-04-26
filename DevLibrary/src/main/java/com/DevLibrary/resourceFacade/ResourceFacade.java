3a6a89c7515b7768073766b7039964ffff1f8008
Hossam alzahrani <hossamal.zahrani944@gmail.com>
2026-04-27 01:43:29 +0300

    complete adding release and close #18

--- DevLibrary/src/main/java/com/DevLibrary/resourceFacade/ResourceFacade.java
+++ DevLibrary/src/main/java/com/DevLibrary/resourceFacade/ResourceFacade.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+//This class provides a simple interface for resource operations.
 @Service
 public class ResourceFacade {
     private ResourceRepository repository;
